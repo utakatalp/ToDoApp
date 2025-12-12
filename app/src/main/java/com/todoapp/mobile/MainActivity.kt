b4fd446beed6525c4ff3eeab5b12378bd66f1d0e
utakatalp <ademalpsahin@hotmail.com>
2025-12-12 15:08:22 +0300

    detekt test

--- app/src/main/java/com/todoapp/mobile/MainActivity.kt
+++ app/src/main/java/com/todoapp/mobile/MainActivity.kt
@@ -12,6 +12,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ToDoAppTheme {
+                        println("TESTTTTTTT DETEKTTT")
+                println("asdşasdasd")
             }
         }
     }
