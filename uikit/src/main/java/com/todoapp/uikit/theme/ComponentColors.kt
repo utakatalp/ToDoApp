41a161468f6e43acbfec5920df1c6fe32c2763a2
Berat Baran <berat_2002_40@hotmail.com>
2025-12-12 17:49:58 +0300

    "ComponentColors.kt has created"

---
+++ uikit/src/main/java/com/todoapp/uikit/theme/ComponentColors.kt
@@ -0,0 +1,26 @@
+package com.todoapp.uikit.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun textFieldColors(): TextFieldColors {
+    return OutlinedTextFieldDefaults.colors(
+        focusedTextColor = MaterialTheme.colorScheme.onSurfaceVariant,
+        focusedTrailingIconColor = MaterialTheme.colorScheme.primary,
+        focusedContainerColor = MaterialTheme.colorScheme.background,
+        focusedLabelColor = MaterialTheme.colorScheme.primary,
+        unfocusedContainerColor = Color.Transparent,
+        errorContainerColor = Color.Transparent,
+        disabledContainerColor = Color.Transparent,
+        disabledTextColor = MaterialTheme.colorScheme.onSurfaceVariant,
+        disabledBorderColor = MaterialTheme.colorScheme.onSurfaceVariant,
+        disabledLabelColor = MaterialTheme.colorScheme.onSurfaceVariant,
+        unfocusedBorderColor = MaterialTheme.colorScheme.onSurfaceVariant,
+        unfocusedLabelColor = MaterialTheme.colorScheme.onSurface,
+        cursorColor = MaterialTheme.colorScheme.primary,
+    )
+}
