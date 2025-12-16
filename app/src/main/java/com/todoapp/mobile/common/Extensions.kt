b361ae17ff5986f01fb9c0568635963d9154e1bc
Berat Baran <berat_2002_40@hotmail.com>
2025-12-16 11:01:40 +0300

    "Extensions.kt has created"

---
+++ app/src/main/java/com/todoapp/mobile/common/Extensions.kt
@@ -0,0 +1,22 @@
+package com.todoapp.mobile.common
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.flow.Flow
+
+@Composable
+fun <T> Flow<T>.CollectWithLifecycle(
+    collect: suspend (T) -> Unit,
+) {
+    val lifecycleOwner = LocalLifecycleOwner.current
+    LaunchedEffect(this, lifecycleOwner) {
+        lifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+            this@CollectWithLifecycle.collect { effect ->
+                collect(effect)
+            }
+        }
+    }
+}
