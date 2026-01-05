3bca62d87d4041e5265d0801fb38bf64ef14214c
Berat Baran <berat_2002_40@hotmail.com>
2026-01-05 20:57:42 +0300

    feat: Add new observation functions to TaskRepository
    New functions have been added to the `TaskRepository` interface to observe task statistics:
    - `observeCompletedCountsByDayInAWeek`
    - `observeCompletedTasksYearToDate`
    - `observePendingTasksYearToDate`

--- app/src/main/java/com/todoapp/mobile/domain/repository/TaskRepository.kt
+++ app/src/main/java/com/todoapp/mobile/domain/repository/TaskRepository.kt
@@ -9,7 +9,10 @@ interface TaskRepository {
     fun observeRange(startDate: LocalDate, endDate: LocalDate): Flow<List<Task>>
     fun observeTasksByDate(date: LocalDate): Flow<List<Task>>
     fun observeCompletedTasksInAWeek(date: LocalDate): Flow<Int>
+    fun observeCompletedCountsByDayInAWeek(date: LocalDate): Flow<List<Pair<LocalDate, Int>>>
     fun observePendingTasksInAWeek(date: LocalDate): Flow<Int>
+    fun observeCompletedTasksYearToDate(date: LocalDate): Flow<Int>
+    fun observePendingTasksYearToDate(date: LocalDate): Flow<Int>
     suspend fun insert(task: Task)
     suspend fun delete(task: Task)
     suspend fun updateTask(id: Long, isCompleted: Boolean)
