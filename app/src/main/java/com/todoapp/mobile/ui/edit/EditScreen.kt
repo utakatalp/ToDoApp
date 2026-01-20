6d8f2b36e16f72557948c4a6d49b0e896019ab75
Berat Baran <berat_2002_40@hotmail.com>
2026-01-20 13:56:19 +0300

    Update padding in EditScreen.kt: replace navigationBarsPadding and statusBarsPadding with systemBarsPadding

--- app/src/main/java/com/todoapp/mobile/ui/edit/EditScreen.kt
+++ app/src/main/java/com/todoapp/mobile/ui/edit/EditScreen.kt
@@ -11,9 +11,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
-import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -81,8 +80,7 @@ fun EditScreen(
             .fillMaxSize()
             .background(TDTheme.colors.background)
             .padding(horizontal = 16.dp, vertical = 8.dp)
-            .navigationBarsPadding()
-            .statusBarsPadding()
+            .systemBarsPadding()
             .imePadding()
     ) {
         Row(
