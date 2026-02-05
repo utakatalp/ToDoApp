4ca1298bc5ff3ad6fc23d9b42787b420d65986f9
utakatalp <ademalpsahin@hotmail.com>
2026-02-05 18:54:07 +0300

    feat: Add SessionPreferences interface and SharedPreferences implementation

---
+++ app/src/main/java/com/todoapp/mobile/domain/repository/SessionPreferences.kt
@@ -0,0 +1,16 @@
+package com.todoapp.mobile.domain.repository
+
+interface SessionPreferences {
+
+    suspend fun setAccessToken(token: String)
+
+    suspend fun getAccessToken(): String?
+
+    suspend fun setRefreshToken(token: String)
+
+    suspend fun getRefreshToken(): String?
+
+    suspend fun setExpiresAt(expiresIn: Long)
+
+    suspend fun getExpiresAt(): Long?
+}
