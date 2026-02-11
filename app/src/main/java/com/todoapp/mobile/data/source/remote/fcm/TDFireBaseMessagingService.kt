03e8c9a0b29775bd48a49af8f6681c868e49052c
Berat Baran <berat_2002_40@hotmail.com>
2026-02-11 15:27:36 +0300

    Add logging to TDFireBaseMessagingService.kt

--- app/src/main/java/com/todoapp/mobile/data/source/remote/fcm/TDFireBaseMessagingService.kt
+++ app/src/main/java/com/todoapp/mobile/data/source/remote/fcm/TDFireBaseMessagingService.kt
@@ -1,5 +1,6 @@
 package com.todoapp.mobile.data.source.remote.fcm
 
+import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -7,9 +8,13 @@ class TDFireBaseMessagingService : FirebaseMessagingService() {
 
     override fun onNewToken(token: String) {
         super.onNewToken(token)
+        Log.d("FCM_TEST", "TOKEN: $token")
     }
 
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
+        Log.d("FCM_TEST", "MESSAGE: ${message.data}")
+        Log.d("FCM_TEST", "MESSAGE: ${message.notification?.body}")
+        Log.d("FCM_TEST", "NOTIF_TITLE: ${message.notification?.title}")
     }
 }
